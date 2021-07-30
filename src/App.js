73cc40003fbe751d4f80c779e33b264bf2fd0280
Alexey Belov <belov_a_v02@mail.ru>
2021-07-30 22:24:31 +0300

    third commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import MyStudentsContainer from "./../src/components/My students/myStudentsContainer"
 import GradeTableContainer from "./components/My students/GradeTableContainer";
 import AddStudent from "./components/My students/buttons/addStudent";
+import SidebarContainer from "./123/SidebarContainer";
 
 const App = (props) => {
     return (
         <div>
+            <SidebarContainer/>
             <MyStudentsContainer/>
             <GradeTableContainer/>
         </div>
